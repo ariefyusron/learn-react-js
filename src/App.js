a767fe2d7c75011bfad0fa1a9b6bd68e7f0c3135
Arief Yusron <ariefyusron98@gmail.com>
2019-09-25 08:02:50 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header title="Header" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Yusron</p>
         <a
           className="App-link"
           href="https://reactjs.org"
